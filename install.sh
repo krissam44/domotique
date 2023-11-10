c62638ea73cd03667a579c0a250395854a176a71
krissam44 <61661831+krissam44@users.noreply.github.com>
2023-11-10 22:38:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -107,7 +107,7 @@ msg "Downloading disk image..."
 wget -q --show-progress $URL
 echo -en "\e[1A\e[0K" #Overwrite output from wget
 FILE=$(basename $URL)
-info "Le fichier downloadé est $FILE."
+msg "Le fichier downloadé est $FILE."
 
 # Extract Home Assistant disk image
 msg "Extracting disk image..."
